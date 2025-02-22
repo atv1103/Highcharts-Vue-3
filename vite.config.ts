a5a907e5d24d8704cf296e4c07534c852b49d22a
Tatiana Anuchina <1103atv@gmail.com>
2025-02-22 18:54:37 +1000

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/Highcharts-Vue-3/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
